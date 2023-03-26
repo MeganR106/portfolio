65b4d495d117aa5bfd0e71ddedd3d5ae79a39069
MeganR106 <106038555+MeganR106@users.noreply.github.com>
2023-03-26 14:17:05 -0500

    updated work experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -23,8 +23,8 @@ import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import hackreactor from "./company/hackreactor.png";
+import katy from "./company/katy.jpg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -54,8 +54,8 @@ export {
   threejs,
   meta,
   shopify,
-  starbucks,
-  tesla,
+  hackreactor,
+  katy,
   carrent,
   jobit,
   tripguide,
