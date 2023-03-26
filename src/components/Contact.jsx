cf93d83ec7a5c773d87b287b4ee28f9fecd5513a
MeganR106 <106038555+MeganR106@users.noreply.github.com>
2023-03-26 12:23:43 -0500

    thank you

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thanks. I will get back to you as soon as possible.");
+          alert("Thank you. I will get back to you as soon as possible.");
 
           setForm({
             name: "",
